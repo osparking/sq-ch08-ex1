e63b8942a89a80af8493f06b3c85e1d4ae1403ce
osparking <jbpark03@gmail.com>
2023-09-21 15:18:12 +0900

    initial with web and thymeleaf

---
+++ src/main/java/space/jbp/sqCh08Ex1/SqCh08Ex1Application.java
@@ -0,0 +1,13 @@
+package space.jbp.sqCh08Ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh08Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh08Ex1Application.class, args);
+	}
+
+}
