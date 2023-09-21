88003c0fca5b7afd6a0f6bb660ea0f8c0535181e
osparking <jbpark03@gmail.com>
2023-09-21 22:20:27 +0900

    in model 2 attributes added

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,12 +1,15 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 	@RequestMapping("/home")
-	public String home() {
+	public String home(Model page) {
+		page.addAttribute("username", "jbpark");
+		page.addAttribute("color", "red");
 		return "home.html";
 	}
 }
