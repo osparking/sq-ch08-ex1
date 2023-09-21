ba421def6189a385486a65da8bf0c96f865b3abc
osparking <jbpark03@gmail.com>
2023-09-21 22:14:19 +0900

    component scan path added

--- src/main/java/space/jbp/sqCh08Ex1/SqCh08Ex1Application.java
+++ src/main/java/space/jbp/sqCh08Ex1/SqCh08Ex1Application.java
@@ -2,8 +2,10 @@ package space.jbp.sqCh08Ex1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("controllers")
 public class SqCh08Ex1Application {
 
 	public static void main(String[] args) {
---
+++ src/test/java/space/jbp/sqCh08Ex1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.jbp.sqCh08Ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
